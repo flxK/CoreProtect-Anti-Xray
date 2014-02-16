bcc0769289c7de5c6ef7a5973541e7e07648f58f
xilefkfelix <dev@felix-kraus.net>
2014-02-16 02:54:42 +0100

    Updated messages and Lists

--- src/net/felixkraus/AntiXray/AntiXray.java
+++ src/net/felixkraus/AntiXray/AntiXray.java
@@ -10,12 +10,11 @@ import net.coreprotect.CoreProtectAPI;
 import net.coreprotect.CoreProtectAPI.*;
 
 import java.text.DecimalFormat;
-import java.util.LinkedList;
 import java.util.List;
 
-/**
- * Created by felix on 14.02.14.
- */
+import static java.util.Arrays.asList;
+
+
 public class AntiXray extends JavaPlugin {
 
     @Override
@@ -29,17 +28,10 @@ public class AntiXray extends JavaPlugin {
                 return false;
             }
             else {
-                List<Integer> ids = new LinkedList<Integer>();
-                ids.add(56);
-                ids.add(14);
-                ids.add(15);
-                ids.add(16);
-                ids.add(129);
-                ids.add(73);
-                ids.add(74);
-                ids.add(21);
-                ids.add(1);
+                List<Integer> ids = asList(1, 14, 15, 16, 21, 56, 73, 74, 129);
+
                 List<String[]> lookup = CPApi.performLookup(args[0], Integer.parseInt(args[1])*86400, 0, null, ids, null);
+
                 int Diamonds = 0;
                 int Iron = 0;
                 int Gold = 0;
@@ -53,28 +45,30 @@ public class AntiXray extends JavaPlugin {
                     if(result.getActionId()==0)
                     switch(result.getTypeId()){
                         case 1 : Stone++; break;
-                        case 56 : Diamonds++; break;
                         case 14 : Gold++; break;
                         case 15 : Iron ++; break;
                         case 16 : Coal++; break;
                         case 21 : Lapis++; break;
-                        case 129 : Emerald++; break;
+                        case 56 : Diamonds++; break;
                         case 73 : Redstone++; break;
                         case 74 : Redstone++; break;
+                        case 129 : Emerald++; break;
                     }
 
                 }
                 int summe = Stone+Diamonds+Gold+Iron+Coal+Lapis+Emerald+Redstone;
                 sender.sendMessage(ChatColor.YELLOW+"----------CP Anti-Xray----------");
                 sender.sendMessage("Total: "+summe);
-                sender.sendMessage(ChatColor.GREEN+"Emeralds: "+Emerald+ percentage(Emerald, summe));
-                sender.sendMessage(ChatColor.AQUA+"Diamonds: "+Diamonds+ percentage(Diamonds, summe));
-                sender.sendMessage(ChatColor.YELLOW+"Gold: "+Gold+ percentage(Gold, summe));
-                sender.sendMessage(ChatColor.DARK_GRAY+"Iron: "+Iron+ percentage(Iron, summe));
-                sender.sendMessage(ChatColor.RED+"Redstone: "+Redstone+ percentage(Redstone, summe));
-                sender.sendMessage(ChatColor.DARK_BLUE+"Lapis Lazuli: "+Lapis+ percentage(Lapis, summe));
-                sender.sendMessage(ChatColor.BLACK+"Coal: "+Coal+ percentage(Coal, summe));
-                sender.sendMessage("Stone: "+Stone+ percentage(Stone, summe));
+
+                sender.sendMessage(messageString(ChatColor.GREEN,       "Emeralds",     Emerald, summe));
+                sender.sendMessage(messageString(ChatColor.AQUA,        "Diamonds",     Diamonds, summe));
+                sender.sendMessage(messageString(ChatColor.YELLOW,      "Gold",         Gold, summe));
+                sender.sendMessage(messageString(ChatColor.DARK_GRAY,   "Iron",         Iron, summe));
+                sender.sendMessage(messageString(ChatColor.RED,         "Redstone",     Redstone, summe));
+                sender.sendMessage(messageString(ChatColor.DARK_BLUE,   "Lapis Lazuli", Lapis, summe));
+                sender.sendMessage(messageString(ChatColor.BLACK,       "Coal",         Coal, summe));
+                sender.sendMessage(messageString(ChatColor.GRAY,        "Stone",        Stone, summe));
+
                 return true;
             }
         }
@@ -85,7 +79,7 @@ public class AntiXray extends JavaPlugin {
 
 
 
-    public static boolean isInteger(String str) {
+    private static boolean isInteger(String str) {
         if (str == null) {
             return false;
         }
@@ -109,15 +103,12 @@ public class AntiXray extends JavaPlugin {
         return true;
     }
 
-
-    public String percentage(int Anzahl, int Summe){
-        double Anz = (double) Anzahl;
-        double Sum = (double) Summe;
-        double zw = Anz/Sum;
-        double perc = zw*100;
-        DecimalFormat f = new DecimalFormat("#0.00");
-        return "  |  "+f.format(perc)+"%";
+    private String messageString(ChatColor color, String Name, int Anzahl, int summe){
+        return color+Name+": "+Anzahl+" "+"  |  "+new DecimalFormat("#0.00").format((double)Anzahl/(double)summe*100)+"%";
     }
+
+
+
 
 
   private CoreProtectAPI getCoreProtect() {
@@ -130,7 +121,7 @@ public class AntiXray extends JavaPlugin {
 
 // Check that the API is enabled
         CoreProtectAPI CoreProtect = ((CoreProtect)pluginCP).getAPI();
-        if (CoreProtect.isEnabled()==false){
+        if (!CoreProtect.isEnabled()){
             return null;
         }
 
